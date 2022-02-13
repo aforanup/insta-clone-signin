4e4a5fa1dcac19c74b2591c33fa8af8fd0b5a473
aforanup <aforanupn@gmail.com>
2022-02-13 15:48:27 +0545

    base is made

---
+++ web_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    phone_no = models.CharField(max_length=15, blank=True)
+    profile_pic = models.ImageField(
+        upload_to='userProfilePic/',
+        null=True, blank=True,
+        verbose_name='profile picture'
+    )
+
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'Users Profile'
+
+    def __str__(self):
+        if self.email:
+            return self.username, self.email
+        else:
+            return self.username
